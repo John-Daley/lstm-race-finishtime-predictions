afd3df391f1dddb88776bae1cd1c502a336aa358
John-Daley <jdaley31415@gmail.com>
2020-02-19 18:03:36 +0100

    finished the very basic lstm for training

--- lstm-test.py
+++ lstm-test.py
@@ -4,7 +4,8 @@ import keras;
 import tensorflow as tf;
 from keras.preprocessing.sequence import TimeseriesGenerator;
 import matplotlib.pyplot as plt
-
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 
 filename = "formated-race-data-for-one.csv"
 downloadedFile =pd.read_csv(filename)
@@ -17,17 +18,17 @@ totalDistanceTraveled = 0;
 timeIntervals = 0;
 currentSpeed= 0;
 timeUntilFinish =0;
-
+currentSpeedForPlotting = []
 for i in range(0, len(timeTraveled)-1):
     totalDistanceTraveled = distanceTraveled[i+1] - distanceTraveled[i]
-    print(totalDistanceTraveled)
+   # print(totalDistanceTraveled)
     timeIntervals = timeTraveled[i] - timeTraveled[i+1]
-    print(timeIntervals)
+  #  print(timeIntervals)
     currentSpeed = totalDistanceTraveled/timeIntervals
     print(str(currentSpeed) + " m/s")
     timeUntilFinish = distanceTraveled[i]/currentSpeed
     print(str(timeUntilFinish/60) + " Minutes until racer is finished")
-
+    currentSpeedForPlotting.append(currentSpeed)
 # for times in range(len(timeTraveled)):
 #   totalDistanceTraveled = distanceTraveled[times+1] - distanceTraveled[times]
 #   print(totalDistanceTraveled + "km")
@@ -39,10 +40,44 @@ for i in range(0, len(timeTraveled)-1):
 # plt.rcParams["figure.figsize"] = fig_size
 
 plt.title('Distance vs Time')
-plt.ylabel('time')
-plt.xlabel('distance')
+plt.ylabel('meters per second')
+plt.xlabel('Distance per km')
 plt.grid(True)
 #plt.autoscale(axis='x',tight=True)
-plt.plot(downloadedFile["time"])
-plt.plot(downloadedFile["Distance-left"])
+#plt.plot(downloadedFile["time"])
+#plt.plot(downloadedFile["Distance-left"])
+plt.plot(currentSpeedForPlotting)
 plt.show()
+
+distanceLeftData = downloadedFile["Distance-left"].values
+distanceLeftData = distanceLeftData.reshape((-1,1))
+
+splitPercent = 0.80
+split = int(splitPercent*len(distanceLeftData))
+
+distanceLeftTrain = distanceLeftData[:split]
+distanceLeftTest = distanceLeftData[split:]
+
+timeTrain = downloadedFile["time"][:split]
+timeTest = downloadedFile["time"][split:]
+
+print(str(len(distanceLeftTrain)) + " distance train")
+print(str(len(distanceLeftTest))+ " distance test")
+
+
+look_back = 4
+
+train_generator = TimeseriesGenerator(distanceLeftTrain, distanceLeftTrain, length=look_back, batch_size=20)
+test_generator = TimeseriesGenerator(distanceLeftTest,distanceLeftTest, length=look_back, batch_size=1)
+
+
+model = Sequential()
+model.add(
+    LSTM(10,activation="relu",input_shape =(look_back,1)
+    )
+)
+model.add(Dense(1))
+model.compile(optimizer="adam", loss="mse")
+
+numberOfEpocs = 30
+model.fit_generator(train_generator, epochs=numberOfEpocs,verbose=1)
