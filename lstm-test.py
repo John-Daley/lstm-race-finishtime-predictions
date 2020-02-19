bb5b971ceef35a15151877fcb84e131264a0aa76
John-Daley <jdaley31415@gmail.com>
2020-02-19 16:12:20 +0100

    added the a bit of code to calculate a single runners finish time that updates dynamically based on there current speed and amount of distance left

--- lstm-test.py
+++ lstm-test.py
@@ -9,19 +9,40 @@ import matplotlib.pyplot as plt
 filename = "formated-race-data-for-one.csv"
 downloadedFile =pd.read_csv(filename)
 print(downloadedFile.info())
+distanceTraveled = downloadedFile["Distance-left"]
+#print(distanceTraveled)
+timeTraveled = downloadedFile["time"]
+#print(timeTraveled)
+totalDistanceTraveled = 0;
+timeIntervals = 0;
+currentSpeed= 0;
+timeUntilFinish =0;
+
+for i in range(0, len(timeTraveled)-1):
+    totalDistanceTraveled = distanceTraveled[i+1] - distanceTraveled[i]
+    print(totalDistanceTraveled)
+    timeIntervals = timeTraveled[i] - timeTraveled[i+1]
+    print(timeIntervals)
+    currentSpeed = totalDistanceTraveled/timeIntervals
+    print(str(currentSpeed) + " m/s")
+    timeUntilFinish = distanceTraveled[i]/currentSpeed
+    print(str(timeUntilFinish/60) + " Minutes until racer is finished")
+
+# for times in range(len(timeTraveled)):
+#   totalDistanceTraveled = distanceTraveled[times+1] - distanceTraveled[times]
+#   print(totalDistanceTraveled + "km")
 
 
-
-
-
-fig_size = plt.rcParams["figure.figsize"]
-fig_size[0] = 15
-fig_size[1] = 5
-plt.rcParams["figure.figsize"] = fig_size
+# fig_size = plt.rcParams["figure.figsize"]
+# fig_size[0] = 15
+# fig_size[1] = 5
+# plt.rcParams["figure.figsize"] = fig_size
 
 plt.title('Distance vs Time')
 plt.ylabel('time')
 plt.xlabel('distance')
 plt.grid(True)
-plt.autoscale(axis='x',tight=True)
-plt.plot(downloadedFile.info())
+#plt.autoscale(axis='x',tight=True)
+plt.plot(downloadedFile["time"])
+plt.plot(downloadedFile["Distance-left"])
+plt.show()
